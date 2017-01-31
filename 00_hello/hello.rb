ecb384a113834a61211f0d65895def836f780f2e
hallitee <hallitee_2005@yahoo.com>
2017-01-30 21:39:59 -0800

    After temperature change

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,9 @@
 #write your code here
+def hello
+
+	"Hello!"
+end 
+
+def greet(text)
+	"Hello, #{text}!"
+end 
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(fahr)
+	((fahr-32)/1.8).round
+end
+
+def ctof(cels)
+	(cels*1.8)+32
+	end
