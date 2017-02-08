10a72f69d1f8e219784380bf8c4eb5fb86bc595e
hallitee <hallitee_2005@yahoo.com>
2017-02-08 01:51:13 -0800

    After finishing the timer spec

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds=0)
+  	@seconds=seconds
+  end
+  def time_string
+  	 t = seconds/60
+  	if t > 60
+  	min = "%02d" % (t % 60)
+  	hour = "%02d" % (t / 60)
+   	sec = "%02d" % (seconds % 60)
+  	else
+  	min = "%02d" % (seconds / 60)
+  	sec = "%02d" % (seconds % 60)
+  	hour = "%02d" % ((min.to_i)/60)
+  end 
+  	"#{hour}:#{min}:#{sec}"
+  end
 end
